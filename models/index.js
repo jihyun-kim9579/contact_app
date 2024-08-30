6c67f943341eb6b7a26c7c6b8f48241ce97dc2a4
kim-ji-hyun <wlgus95799@naver.com>
2024-08-30 18:06:27 +0900

    메모 모델 구현 및 관계설정

--- models/index.js
+++ models/index.js
@@ -5,7 +5,10 @@ dotenv.config();
 import config from "../config/config.json" assert {type:'json'};
 const env = process.env.NODE_ENV || "development";
 const configEnv = config[env]; // username , database , password , host .. 여기안에 다 있음.
-console.log(env);
+
+import User from "./User.js"; //User 모델 불러오기
+import Memo from "./Memo.js";
+
 
 // import User from "./User.js";
 
@@ -17,31 +20,35 @@ const sequelize = new Sequelize (configEnv.database, configEnv.username, configE
   logging: (msg) => console.log(msg)
 });
 
+User.init(sequelize); // User 모델의 컬럼, 자료형 --> MariaDB에 생성
+Memo.init(sequelize);
 //DB 연결 테스트
-try {
-  await sequelize.authenticate();
-  console.log('Connection has been established successfully.');
-} catch (error) {
-  console.error('Unable to connect to the database:', error);
-}
+// try {
+//   await sequelize.authenticate();
+//   console.log('Connection has been established successfully.');
+// } catch (error) {
+//   console.error('Unable to connect to the database:', error);
+// }
 
 // Model - Table 동기화 (Synchroniazation) == > Promises 객체를 반환 : 비동기
-await sequelize.sync({ force: true }).then(() => {
-  console.log('All models were synchronized successfully.'); // 메세지는 출력되었으나 users 테이블은 아직 생성 전
+await sequelize.sync({ force: false }).then(() => {
+  console.log('===========모델과 데이터베이스가 매핑되었습니다. =========='); // 메세지는 출력되었으나 users 테이블은 아직 생성 전
 }).catch((error) => {
-  console.log('Error Occured' + error);
+  console.log('===========에러내용 ' + error);
 });
 
 
 // DB 정의
 const db = {};
 // 객체.속성 = "값"
-import User from "./User.js";
 
 db.sequelize = sequelize; //시퀄라이즈 인스턴스 (DB 접속정보)
 db.User = User;
-User.init(sequelize); // User 모델의 컬럼, 자료형 --> MariaDB에 생성
+db.Memo = Memo;
 
+
+User.associate(db);
+Memo.associate(db);
 export default db;
 
 
