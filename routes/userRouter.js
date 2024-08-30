96b940345284d5d91f4aab116deef3309d6f47ef
kim-ji-hyun <wlgus95799@naver.com>
2024-08-30 18:06:33 +0900

    라우터 완료.

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -10,6 +10,14 @@ const timeLog = (req, res, next) => {
 router.use(timeLog)
 
 // User(연락처) 생성
+router.get('/user/:id', userControl.findOneUser);   // 동적파라미터
 router.post('/', userControl.createUser);
 
+//Users(연락처) 조회
+router.get('/users', userControl.findAllUsers);
+router.delete('/users', userControl.removeAllUsers);
+router.delete('/user/:id', userControl.removeUser);
+
+//연락처 수정
+router.put('/user', userControl.updateUser);
 export default router;
